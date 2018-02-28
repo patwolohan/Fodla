10d1d49594eb467a7d8bd1b7d84e9041a66c9ab1
patwolohan <pwolohan@gmail.com>
2018-02-28 09:51:07 +0000

    added JOpt and parameters -v -help - version

---
+++ run.command
@@ -0,0 +1,9 @@
+@ECHO	OFF
+REM	---------------------------------------------------------------------
+REM	the	directory	path	to	java.exe	on	your	machine	may	be	different
+REM	from	the	path	I	have	between	the	double	quotes	below	to	this	may
+REM	need	to	be	changed	in	order	for	compile	to	work	on	your	machine
+REM	IF	JAVA_HOME	is	set	you	do	not	need	to	specify	the	path	just	java
+REM	---------------------------------------------------------------------
+"C:\Program	Files\Java\jdk1.8.0_162\bin\java.exe" java -cp target/Fodla-v1.jar com.patwolohan.Fodla.App
+PAUSE
--- src/main/java/com/patwolohan/Fodla/App.java
+++ src/main/java/com/patwolohan/Fodla/App.java
@@ -1,14 +1,21 @@
-package com.patwolohan.Fodla;
-
+package	com.patwolohan.Fodla;
+import	java.io.File;
+import	java.io.IOException;
+import	java.util.Arrays;
 import	java.util.Date;
 import	java.util.Scanner;
+import	org.apache.logging.log4j.Level;
 import	org.apache.logging.log4j.LogManager;
 import	org.apache.logging.log4j.Logger;
+import	org.apache.logging.log4j.core.config.Configurator;
+import	joptsimple.OptionException;
+import	joptsimple.OptionParser;
+import	joptsimple.OptionSet;
 
 /*****************************************************************
  *
- *	 	Date:	2017
- *	 	@author	PW
+ *	 Date:	2018
+ *	 @author	PW
  *
  *	
  *	The	purpose	of	this	application	is	to	provide	an	example	for	the	following:
@@ -21,8 +28,6 @@ import	org.apache.logging.log4j.Logger;
  *	
  *****************************************************************/
 public	class App{
-
-
 	public	static void	main(	String[]	args	){
 		//	To	view	the	arguments	being	entered
 		seeCommandlineInput(args);
@@ -33,17 +38,23 @@ public	class App{
 	//	DATA
 	//............................................................
 	//define	attributes
+	
 	private	 Scanner	someInput;
 	private	Date	today;
 	//	This	is	added	to	every	class	that	needs	to	log	with	one	change
 	//	The	getLogger(	)	part	should	contain	the	name	of	the	class	its	in
 	private	static	Logger	LOG;
+	
 	//	CONSTRUCTORS
 	//............................................................
-	public	App(){	 
+	public	App(	Level	logLevel	){	
 		
 		//associate	logging	with	this	class	so	know	the	messages	that	came	from	objects	of	this	class
 		LOG	=	LogManager.getLogger(App.class);
+		Configurator.setLevel(LOG.getName(),	logLevel);
+		//	Check	the	log	level	requested
+		LOG.info("Commandline	requested	log	level:"	+	logLevel	);	 	
+		LOG.info("Application	started	with	log	level	debug:"	+	LOG.isDebugEnabled());
 		//test	the	logging
 		testLogOutput();
 		this.someInput	=	new	Scanner(System.in);
@@ -56,6 +67,10 @@ public	class App{
 		System.exit(0);
 	}
 	
+	public	App(){
+		this(	Level.INFO	);
+	}
+	
 	//	METHODS	used	by	main()	or	debug	methods	-	note	they	are	static	methods
 	//............................................................
 	/**
@@ -64,14 +79,61 @@ public	class App{
 	 */
 	private	static void	actionCommandlineInput(	String	args[]	){
 		
-		//	no	special	instantiation	yet	as	don't	pass	args	to	it
-		App	anApp	=	new	App();
-	}
+		try{	
+			final	OptionParser	optionParser	=	new	OptionParser();
+			//define	the	allowed	arguments
+			optionParser.acceptsAll(Arrays.asList("v",	"verbose"),	"Set	logging	level	to	DEBUG	to	see	all	levels	of	log	messages").forHelp();
+			optionParser.acceptsAll(Arrays.asList("h",	"help"),	"Display	help/usage	information").forHelp();
+			optionParser.acceptsAll(Arrays.asList("r",	"version"),	"Display	program	version	information").forHelp();
+			final	OptionSet	options	=	optionParser.parse(args);
+			
+			if	(options.has("help")){
+				System.out.println("This	program	takes	an	SQL	database	with	a	User	table	as	displays	the	users.");
+				System.out.println("It	is	provided	as	an	example	for	teaching	Java	programming.");
+				printUsage(optionParser);
+				System.exit(0);
+			}
+			
+			if	(options.has("version")){
+				System.out.println("Fodla	version	0.3");
+				System.exit(0);
+			}
+			//	valid	input	so	start	the	program	with	the	name	of	the	database	file	to	use
+			
+			if	(options.has("verbose")	){
+				
+				Level	logLevel	=	Level.DEBUG;
+				System.out.println("RUN	WITH:	logging	level	requested:	"	+	logLevel);
+				App	anApp	=	new	App(logLevel);
+			}else{
+				
+				System.out.println("RUN	WITH:	logging	level	requested:	"	+	Level.INFO);
+				App	anApp	=	new	App();
+			}
+		}catch	(OptionException	argsEx){
+			System.out.println("ERROR:	Arguments\\parameter	is	not	valid.	"	+	argsEx);
+		}
+	}//EOM
+	
+	/**
+	 *	Write	help	message	to	standard	output	using
+	 *	the	provided	instance	of	{@code	OptionParser}.
+	 */
+	private	static void	printUsage(final	OptionParser	parser){
+		try{
+			parser.printHelpOn(System.out);		
+		}catch	(IOException	ioEx){
+			//	System.out.println("ERROR:	Unable	to	print	usage	-	"	+	ioEx);
+			LOG.error("ERROR:	Unable	to	print	usage	-	"	+	ioEx);
+		}
+	}//EOM
+	
 	/**
 	 *	View	the	arguments	presented	at	the	commandline
 	 *	This	is	for	debug	and	demo	purposes
 	 */
-	private	static void	seeCommandlineInput(	String	args[]	){	
+	
+	private	static void	seeCommandlineInput(	String	args[]	){
 		
 		if	(args.length	==	0){
 			
@@ -88,7 +150,6 @@ public	class App{
 	/**
 	 *	Test	the	Log4J2	logging
 	 */
-	
 	private	static void	testLogOutput(){
 		
 		LOG.debug("Log	test:	Test	printed	on	debug");
